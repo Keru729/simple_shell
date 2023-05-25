abdd3f650204f3eb37e8f004ec70c68287d3c4fc
Kerubel Solomon <kibi729@gmail.com>
2023-05-25 13:05:53 -0700

    olp

--- main.c
+++ main.c
@@ -1,28 +1,44 @@
 #include "shell.h"
 
 /**
-* main - entry point
-* @ac: arg count
-* @av: arg vector
-*
-* return: 0 on success, 1 on error
-*/
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
 int main(int ac, char **av)
 {
-	info_t sa[] = { INFO_INIT };
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
+
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
 
 	if (ac == 2)
 	{
-		sa->readfd = open_file(sa, av[1], 0);
-		if (sa->readfd == -1)
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
 		{
-			free_info(sa, 1);
-			exit(sa->err_num);
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
 		}
+		info->readfd = fd;
 	}
-	populate_env_list(sa);
-	read_history(sa);
-	read_startup_file(sa);
-	hsh(sa, av);
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
 	return (EXIT_SUCCESS);
 }
